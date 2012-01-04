b14e5593f2cb76a08f8dd2d8aa61d8bd707e4829
Glen Chiacchieri <www@glench.com>
2012-01-04 14:53:16 -0500

    Use correct method to install gitignore

--- install.sh
+++ install.sh
@@ -2,6 +2,3 @@
 # have to pattern match because * won't find dot files
 cp -r dotfiles/.[a-zA-Z0-9]* ~/
 crontab cron
-
-# ignore these filetypes for all git projects
-git config --global core.excludesfile ~/.gitignore
