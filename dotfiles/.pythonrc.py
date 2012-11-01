79238d7c9506b93695b4b3c8027d9c8b6c3789aa
Glen Chiacchieri <www@glench.com>
2012-11-01 01:30:47 -0400

    add readline and readline completer to pythonrc

--- dotfiles/.pythonrc.py
+++ dotfiles/.pythonrc.py
@@ -1,2 +1,9 @@
 # run these on python shell startup
 from datetime import datetime, date, time, timedelta
+try:
+    import readline
+except ImportError:
+    print "Module readline not available."
+else:
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
