e5e91e4f4a885ca163f70dd0af25c4ff6667ca82
Glen Chiacchieri <www@glench.com>
2020-01-26 20:42:28 -0500

    install nnn as `ld` on command line, also have fzf installed

--- install.sh
+++ install.sh
@@ -2,3 +2,11 @@
 # have to pattern match because * won't find dot files
 cp -r dotfiles/.[a-zA-Z0-9]* ~/
 crontab cron
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    brew install nnn fzf
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+    sudo apt-get install nnn 
+fi
